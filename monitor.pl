e78fd2733f0320d60c73cea77aed39ef62271128
Nathan Campos <nathan@innoveworkshop.com>
2025-11-23 17:26:18 +0000

    Created a script to monitor a list of sites automatically
    This new script is responsible for automating the check of sites based
    on the configuration file (config.json).

---
+++ monitor.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+
+package NetCheck::Monitor;
+
+use strict;
+use warnings;
+use autodie;
+
+use Data::Dumper;
+use File::Basename;
+
+require "./check.pl";
+
+# Checks all monitored sites.
+sub check_monitored {
+	my ($timeout, @monitored) = @_;
+	my $failures = 0;
+
+	# Go through each monitored site.
+	foreach my $site (@monitored) {
+		$failures += NetCheck::Check::check($site->{'proto'}, $site->{'url'}, $timeout);
+	}
+
+	return $failures;
+}
+
+# Script's main entry point.
+sub main {
+	# Load configuration and check monitored sites.
+	my $config = NetCheck::Check::load_config();
+	return check_monitored($config->{'timeout'}, @{$config->{'monitored'}});
+}
+
+# Run main sub-routine if called as a script.
+if (caller) {
+	1;
+} else {
+	__PACKAGE__->main();
+}
+
+__END__;
